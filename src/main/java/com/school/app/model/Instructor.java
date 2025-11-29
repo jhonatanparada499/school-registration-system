a9d216c961f43803a77a0382a8c2dfb8845bb2f7
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-11-29 14:11:14 -0500

    feat: add private instance variables

--- src/main/java/com/school/app/model/Instructor.java
+++ src/main/java/com/school/app/model/Instructor.java
@@ -1,5 +1,13 @@
 package com.school.app.model;
 
+import java.util.List;
+
 public class Instructor {
+  private String id;
+  private String name;
+  // contains courses ids
+  private List<String> qualifiedCourses;
+  // classes currently being taught
+  private List<ClassSession> teachingAssignment;
 
 }
