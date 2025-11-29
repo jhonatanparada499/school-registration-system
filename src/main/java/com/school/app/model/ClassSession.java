93356c6a0fb00a8777602aa7c9b38396fda6c05e
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-11-29 13:04:03 -0500

    feat: add private variables

--- src/main/java/com/school/app/model/ClassSession.java
+++ src/main/java/com/school/app/model/ClassSession.java
@@ -1,5 +1,12 @@
 package com.school.app.model;
 
-public class ClassSession {
+import java.util.List;
 
+public class ClassSession {
+  private Course course;
+  private Instructor instructor;
+  private Classroom classroom;
+  private int sectionNumber;
+  private int maxCapacity;
+  private List<Student> enrolledStudents;
 }
