2a71c71ffa2bf3357218be2c26e36984b0f3b8f6
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-11-29 17:05:29 -0500

    feat: add definition for findEligibleInstructur method

--- src/main/java/com/school/app/service/RegistrationService.java
+++ src/main/java/com/school/app/service/RegistrationService.java
@@ -1,5 +1,26 @@
 package com.school.app.service;
 
+import com.school.app.model.Instructor;
+import com.school.app.model.Course;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+
 public class RegistrationService {
 
+  public List<Instructor> findEligibleInstructors(Course theCourse) {
+    List<Instructor> eligibleInstructors = new ArrayList<>();
+
+    // get Instructor objects without key from hashmap
+    Collection<Instructor> instructors = InstructorService.load().values();
+
+    for (Instructor instructor : instructors) {
+      if (instructor.canTeach(theCourse)) {
+        eligibleInstructors.add(instructor);
+      }
+    }
+
+    return eligibleInstructors;
+  }
 }
