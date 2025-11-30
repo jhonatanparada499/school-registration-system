1946e46bc9d6aee5f5c53c955941853d6fd68505
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-11-30 13:45:51 -0500

    feat: create simple test case for Main.java

---
+++ src/test/java/com/school/app/MainTest.java
@@ -0,0 +1,16 @@
+package com.school.app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MainTest {
+
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
