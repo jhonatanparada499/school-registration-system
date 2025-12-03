5bbfa891e950c0905c7c7d92552b0f2ab878b83b
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-12-02 19:52:36 -0500

    feat: demo program in CLI version

--- src/main/java/com/school/app/Main.java
+++ src/main/java/com/school/app/Main.java
@@ -2,9 +2,10 @@ package com.school.app;
 
 // import com.school.app.service.RegistrationService;
 // import com.school.app.model.Course;
+import com.school.app.view.cli.*;
 
 public class Main {
   public static void main(String[] args) {
-    // Create Dashboard and Administration
+    MainMenu.display();
   }
 }
