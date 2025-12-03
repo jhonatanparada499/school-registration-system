eabd6fa9bc3d42c1d6fa77620aeaef8da7d3c5f5
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-12-02 19:50:30 -0500

    feat: define logic for creation of a class section

--- src/main/java/com/school/app/view/cli/Administration.java
+++ src/main/java/com/school/app/view/cli/Administration.java
@@ -3,11 +3,17 @@ package com.school.app.view.cli;
 import java.util.Scanner;
 
 import java.util.Map;
-import java.util.HashMap;
+import java.util.List;
+
+import com.school.app.service.InstructorService;
+import com.school.app.service.RegistrationService;
+import com.school.app.service.ClassroomService;
+import com.school.app.service.CourseService;
 
 import com.school.app.model.Instructor;
-import com.school.app.service.InstructorService;
 import com.school.app.model.ClassSession;
+import com.school.app.model.Course;
+import com.school.app.model.Classroom;
 
 public class Administration {
   public static void display() {
@@ -18,14 +24,18 @@ public class Administration {
     System.out.println("2. Register Student");
     System.out.println("3. Go Back");
 
+    System.out.print("Your choice: ");
     userOption = keyboard.nextInt();
     switch (userOption) {
       case 1:
-        //
+        ClassSectionCreator.display();
+        break;
       case 2:
-        //
+        System.out.println("Still working in this area");
+        System.exit(0);
+        break;
       case 3:
-        //
+        MainMenu.display();
         break;
 
       default:
@@ -42,30 +52,72 @@ public class Administration {
       Scanner keyboard = new Scanner(System.in);
       String courseIdChoice = keyboard.next();
 
-      // Need CourseService.java
-      // Then use it to the a Course object to use
-      // RegistrationService.findElegibleInstructor()
+      // get a list of eligibleInstructors for the course choice
+      Map<String, Course> courses = CourseService.load();
+      Course courseChoice = courses.get(courseIdChoice);
+      List<Instructor> eligibleInstructors = RegistrationService.findEligibleInstructors(
+          courseChoice);
 
-      System.out.print("Enter Instructor ID: ");
-      String instructorIdChoice = keyboard.next();
+      // print elebigle instructors ID: Name
+      System.out.println("List of available instructors: ");
+      System.out.println("ID : Name");
+      for (Instructor instructor : eligibleInstructors) {
+        // System.out.println(instructor);
+        System.out.println(
+            instructor.getId() + " : "
+                + instructor.getName());
+      }
 
-      // Checks if the teaching assingment credits of the instructor
-      // is less or equal than 9, otherwise it succeedes and add the course
-      // to the instructor teachingAssignment
-      // ----------------------
-      // Instructor instructorChoice = qualifiedInstructors.get(instructorIdChoice);
-      // if (instructorChoice.getCurrentLoad() <= 9) {
-      // // To create classesssion, I need course, classroom, sectionnumber,
-      // // and maxCapacity
-      //
-      // // ClassSession classSession =
-      // // new ClassSession(theCourse, theInstructor, theClassroom, theMaxCapacity)
-      // System.out.println(
-      // "Success! " + instructorChoice +
-      // " has been assigned" + courseIdChoice);
-      // }
-      // ---------------------------
+      // get an instructor that meets credit condition based on id choice
+      boolean validInstructorChoice = false;
+      while (!validInstructorChoice) {
+        System.out.print("Enter Instructor ID: ");
+        String instructorIdChoice = keyboard.next();
+        Map<String, Instructor> instructors = InstructorService.load();
+        Instructor instructorChoice = instructors.get(instructorIdChoice);
 
+        if (instructorChoice.getCurrentLoad() > 9) {
+          System.out.println("That instructor surpassed credit limit");
+          System.out.println("Please try another one");
+          continue;
+        }
+
+        validInstructorChoice = true;
+
+        Map<String, Classroom> classrooms = ClassroomService.load();
+        System.out.println("List of available classrooms: ");
+        System.out.println("Room Number | Has Computer | Has smartboard");
+
+        // print availbe rooms
+        for (Classroom classroom : classrooms.values()) {
+          System.out.println(
+              classroom.getRoomNumber() + " | " +
+                  classroom.hasComputer() + " | " +
+                  classroom.hasSmartboard());
+        }
+        System.out.print("Enter Room Number: ");
+        String classroomNumberChoice = keyboard.next();
+        Classroom classroomChoice = classrooms.get(classroomNumberChoice);
+
+        System.out.print("Enter Classroom Maximum Capacity: ");
+        int maxCapacityChoice = keyboard.nextInt();
+
+        RegistrationService newRegistration = new RegistrationService();
+        try {
+          ClassSession newClassSection = newRegistration.createClassSection(
+              courseChoice,
+              instructorChoice,
+              classroomChoice,
+              maxCapacityChoice);
+          // Writes the new class section record to ClassSession.csv
+          RegistrationService.saveClassSection(newClassSection);
+          System.out.println("Success");
+        } catch (Exception e) {
+          System.out.println(e);
+          System.out.println("Failure");
+        }
+
+      }
       keyboard.close();
 
     }
