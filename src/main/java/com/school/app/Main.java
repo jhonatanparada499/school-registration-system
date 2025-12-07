efd7c634bb4e5e90dd54b42a887b7f35acc482d7
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-12-07 18:55:39 -0500

    feat: invoke intial scene or graphical interface for app

--- src/main/java/com/school/app/Main.java
+++ src/main/java/com/school/app/Main.java
@@ -1,40 +1,38 @@
 package com.school.app;
 
-// import javafx.application.Application;
-// import javafx.scene.Scene;
-// import javafx.scene.control.Label;
-// import javafx.scene.layout.StackPane;
-// import javafx.stage.Stage;
-//
-// public class Main extends Application {
-//
-// @Override
-// public void start(Stage primaryStage) {
-// primaryStage.setTitle("Hello World!");
-//
-// Label helloLabel = new Label("Hello, JavaFX!");
-//
-// // Create a StackPane as the root layout for the scene
-// // StackPane centers its children by default
-// StackPane root = new StackPane();
-// root.getChildren().add(helloLabel); // Add the label to the StackPane
-//
-// // Create a Scene with the root layout and specified dimensions
-// Scene scene = new Scene(root, 300, 200); // Width 300, Height 200
-//
-// primaryStage.setScene(scene);
-//
-// primaryStage.show();
-// }
-//
-// public static void main(String[] args) {
-// launch(args);
-// }
-// }
-import com.school.app.view.cli.MainMenu;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import java.net.URL;
 
-public class Main {
+/**
+ *
+ * Inspiration: https://www.vojtechruzicka.com/javafx-fxml-scene-builder/
+ */
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage primaryStage) {
+
+    FXMLLoader loader = new FXMLLoader();
+    URL xmlUrl = getClass().getResource("/com/school/app/view/mainScene.fxml");
+    loader.setLocation(xmlUrl);
+
+    try {
+      Parent root = loader.load();
+      primaryStage.setTitle("School Registration System");
+      primaryStage.setScene(new Scene(root));
+      primaryStage.show();
+    } catch (Exception e) {
+      System.out.println("Error from loader");
+      System.out.println(e);
+    }
+  }
+
   public static void main(String[] args) {
-    MainMenu.display();
+    launch();
   }
 }
