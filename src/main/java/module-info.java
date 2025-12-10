beaa83ed6ebe9bfa21a55581c80b68e593a6d6d2
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-12-09 21:16:44 -0500

    fix: allow compatibility between fxml files and java code for the model package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.school.app {
   opens com.school.app.controller to javafx.fxml;
 
   opens com.school.app to javafx.graphics;
+  opens com.school.app.model to javafx.base;
 
   exports com.school.app;
 }
